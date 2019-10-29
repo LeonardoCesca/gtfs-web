227a1cc0d9560f00eb1b198a66f014eaef16b2c2
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-29 14:04:15 -0300

    UPDATE project with new .txt

--- reader.php
+++ reader.php
@@ -2,16 +2,15 @@
 
 $conn = mysqli_connect('localhost','root','','demo');
 
-$open = fopen('employee-data.txt','r');
+$open = fopen('routes.txt','r');
 
 while (!feof($open)) 
 {
 	$getTextLine = fgets($open);
-	$explodeLine = explode(",",$getTextLine);
 	
-    list($name,$city,$postcode,$job_title) = $explodeLine;
+    list($route_id, $agency_id, $route_short_name, $route_long_name, $route_desc, $route_type, $route_url, $route_color, $route_text_color) = array_pad(explode(",",$getTextLine), 9, null);
     	
-	$qry = "insert into empoyee_details (name, city,postcode,job_title) values('".$name."','".$city."','".$postcode."','".$job_title."')";
+	$qry = "insert into routes (route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) values('".$route_id."','".$agency_id."','".$route_short_name."','".$route_long_name."','".$route_desc."','".$route_type."','".$route_url."','".$route_color."','".$route_text_color."')";
 	mysqli_query($conn,$qry);
 }
 
