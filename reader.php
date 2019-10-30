12e3ec0b8aabfba24e237dee9fdc8c7c32d02def
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-30 20:30:22 -0300

    UPDATE with pdo connection

--- reader.php
+++ reader.php
@@ -7,12 +7,14 @@ $open = fopen('routes.txt','r');
 while (!feof($open)) 
 {
     $getTextLine = fgets($open);
-    	
-    $list_explode = list($route_id, $agency_id, $route_short_name, $route_long_name, $route_desc, $route_type, $route_url, $route_color, $route_text_color) = array_pad(explode(",",$getTextLine), 9, null);
         
-    echo $list_explode;
+    $explodeLine = explode(",",$getTextLine);
 
+    $list_explode = list($route_id, $agency_id, $route_short_name, $route_long_name, $route_desc, $route_type, $route_url, $route_color, $route_text_color) = array_pad($explodeLine, 9, null);
+        
     $qry = $pdo->prepare("INSERT INTO routes (route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) values('".$route_id."','".$agency_id."','".$route_short_name."','".$route_long_name."','".$route_desc."','".$route_type."','".$route_url."','".$route_color."','".$route_text_color."')");
+    
+    $qry->execute($list_explode);
    
 }
 
