e84120ecf01af51cec319d2eff8cbe284f3b8818
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-30 16:17:56 -0300

    update

--- reader.php
+++ reader.php
@@ -22,8 +22,17 @@ while (!feof($open))
 	
     list($route_id, $agency_id, $route_short_name, $route_long_name, $route_desc, $route_type, $route_url, $route_color, $route_text_color) = array_pad(explode(",",$getTextLine), 9, null);
     	
-	$qry = "insert into routes (route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) values('".$route_id."','".$agency_id."','".$route_short_name."','".$route_long_name."','".$route_desc."','".$route_type."','".$route_url."','".$route_color."','".$route_text_color."')";
-	mysqli_query($conn,$qry);
+	$qry = $pdo->prepare("INSERT INTO routes (route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) values('".$route_id."','".$agency_id."','".$route_short_name."','".$route_long_name."','".$route_desc."','".$route_type."','".$route_url."','".$route_color."','".$route_text_color."')");
+    
+    $qry->bindParam(:route_id, $route_id);
+    $qry->bindParam(:agency_id, $agency_id);
+    $qry->bindParam(:route_short_name, $route_short_name);
+    $qry->bindParam(:route_long_name, $route_long_name);
+    $qry->bindParam(:route_desc, $route_desc);
+    $qry->bindParam(:route_type, $route_type);
+    $qry->bindParam(:route_url, $route_url);
+    $qry->bindParam(:route_color, $route_color);
+    $qry->bindParam(:route_text_color, $route_text_color);
 }
 
 fclose($open);
