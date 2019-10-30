cf9477f98da923236a2e23a1b7e4170244740033
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-30 16:09:29 -0300

    initial rewrite to PDO

--- reader.php
+++ reader.php
@@ -1,12 +1,24 @@
 <?php 
 
-$conn = mysqli_connect('localhost','root','','demo');
+$conexao = "mysql:host=localhost;dbname=trabWeb";
+$user = "root";
+$password= "";
+$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $pdo = new PDO($conexao, $user, $password);
+    echo "Connected successfully";
+} else (PDOException $error) {
+    echo "Connection error: " . $error->getMessage();
+}
 
 $open = fopen('routes.txt','r');
 
 while (!feof($open)) 
 {
-	$getTextLine = fgets($open);
+    $getTextLine = fgets($open);
+    
+    echo $getTextLine;
 	
     list($route_id, $agency_id, $route_short_name, $route_long_name, $route_desc, $route_type, $route_url, $route_color, $route_text_color) = array_pad(explode(",",$getTextLine), 9, null);
     	
