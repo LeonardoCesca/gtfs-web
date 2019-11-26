7c255b5688f8964431b1ccb56a836b39e3abe666
LeonardoCesca <leonardocescaf@gmail.com>
2019-11-26 18:11:21 -0300

    ADD responsive

--- table.php
+++ table.php
@@ -26,6 +26,7 @@ try {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />    
         <link rel="stylesheet" href="./assets/css/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="./assets/css/style.css">
         <title>Informações sobre Onibus</title>
